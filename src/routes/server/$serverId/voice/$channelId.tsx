05c0df793188c4ffe59514a7f415ffd6a635d9b8
2043299 <2043299@cegepmv.ca>
2026-04-01 16:37:04 -0400

    fix last commit

--- src/routes/server/$serverId/voice/$channelId.tsx
+++ src/routes/server/$serverId/voice/$channelId.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { VoiceChannel } from '../../../../features/voice/VoiceChannel';
-import { Id } from '../../../../../convex/_generated/dataModel';
+import { VoiceChannel } from '@/features/voice/VoiceChannel';
 
 export const Route = createFileRoute('/server/$serverId/voice/$channelId')({
   component: RouteComponent,
@@ -8,5 +7,5 @@ export const Route = createFileRoute('/server/$serverId/voice/$channelId')({
 
 function RouteComponent() {
   const { serverId, channelId } = Route.useParams();
-  return <VoiceChannel serverId={serverId as Id<"servers">} channelId={channelId as Id<"channels">} />;
+  return <VoiceChannel serverId={serverId as any} channelId={channelId as any} />;
 }
